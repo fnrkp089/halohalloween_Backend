648f4d8100fcbfb700779604cd579a6cda692fd8
Seonho AN <seonho.an.dev@gmail.com>
2021-10-13 13:04:31 +0900

    댓글기능API

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 connect();
 app.use('/user', [userRouter]);
 app.use('/post', [postRouter]);
+app.use('/reply', [replyRouter]);
+
 
 app.listen(port, () => {
     console.log(`Server On http://localhost:${port}`);
--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,7 +3,6 @@ const User = require('../schemas/user');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-<<<<<<< HEAD
     try {
         const { authorization } = req.headers;
         const [tokenType, tokenValue] = authorization.split(" ");
@@ -25,26 +24,4 @@ module.exports = (req, res, next) => {
         });
         return;
     }
-=======
-   try {
-      const { authorization } = req.headers;
-      const [tokenType, tokenValue] = authorization.split(' ');
-      if (tokenType !== 'Bearer') {
-         res.status(401).send({
-            errorMessage: "로그인 후 이용 가능한 기능입니다.",
-         });
-         return;
-      }
-      const { userID } = jwt.verify(tokenValue, process.env.TOKEN_KEY);
-      User.findById( userID ).then((user) => {
-         res.locals.user = user;
-         next();
-      });
-   } catch (error) {
-      res.json({
-         Message: '토큰에러, 토근 확인 요망'
-      });
-      return;
-   }
->>>>>>> 3d2f3f8f34970097f1c3fa790810a34e1a407395
 };
