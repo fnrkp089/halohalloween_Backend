c46f4967970d1d5548fdfc7ce95575d3556de14c
Sung Hyun Hong (Jenny) <57204918+Jenny9197@users.noreply.github.com>
2021-10-20 02:26:27 +0900

    성현님 화이팅화이팅

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -29,10 +29,10 @@ Router.get('/postlist', async(req, res) => {
   }
 })
 
-//베스트 게시물 불러오기 API
+//게시물 불러오기 API
 Router.get('/postBest', async(req, res) => {
   try {
-    const postList = await Board.find({postingDel: 1}).sort({"postingSeen": -1}).limit(4);
+    const postList = await Board.find().sort("-postingSeen").limit(4);
     res.status(201).json({ postList : postList });
   } catch (err) {
     res.status(400).send({
@@ -49,15 +49,10 @@ Router.post('/posting', authMiddleWare, async(req, res) => {
   try {
     const posting = new Board({postingTitle, postingEmail:user.userEmail ,postingAuthor: user.userNickname, 
       postingDate, postingComment, postingImgUrl, postingSeen:0,
-      postingDel});
-      await posting.save();
-      const postingOne = await Board.findOne().sort({'_id': -1});// 새로추가됨
-      //리덕스의 상태를 변화시키기 위해 메세지만 보내는 것이 아닌 방금 등록한 글을 바로 보내주어
-      //리덕스에 추가시켜준다. 따라서 화면에 그려지는 부분은 리덕스가 변화되었기 때문에
-      //새로 갱신되게 된다.
-    res.status(201).json({
-      Message: '게시글 등록에 성공했습니다', 
-      result: postingOne
+      postingDel})
+    await posting.save();
+    res.status(201).send({ 
+      message: "게시글 등록을 완료했습니다."
     });
   } catch (err) {
       res.status(500).send({ 
@@ -103,4 +98,3 @@ Router.patch('/postClick', async(req, res) => {
 
 
 module.exports = Router;
-
